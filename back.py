3db251629080f73792ac7a1d10fd1d1b86a45bae
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-06-16 15:12:30 -0300

    chat bot

--- back.py
+++ back.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template, request
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -15,6 +24,31 @@ def ajuda():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+
+@app.route('/resposta', methods=['POST'])
+def resposta():
+    conteudo = 'Você é um medico com mais de 50 anos de experiencia'
+    pergunta = request.form['pergunta']
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": conteudo},
+            {"role": "user", "content": pergunta},
+            {"role": "assistant", "content": 'Ajude estudantes universitario da faculdade de medicina que fizerem perguntas'}
+        ],
+        model="llama3-8b-8192",
+        temperature=0,
+        max_tokens=1024,
+        top_p=1,
+        stop=None,
+    )
+    
+    resposta = chat_completion.choices[0].message.content
+
+    return render_template('chat.html', resposta=resposta, pergunta=pergunta)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
